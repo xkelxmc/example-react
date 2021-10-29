c2b22374d7b62f02fb4af1e730ce10484963d169
Ilya Zhidkov <xkelxmc@yandex.ru>
2021-10-29 17:00:56 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import {Header} from "./components/Header";
 import {Counter} from "./components/Counter";
 import {Footer} from "./components/Footer";
 import {TextInput} from "./components/TextInput";
+import {useState} from "react";
 
 const App = () => {
+  const [show, setShow] = useState(true)
+  const [intValue, setIntValue] = useState(0)
+
+  const handleClick = () => {
+    setShow(prev => !prev);
+  }
+
+  const handleClickChangeValue = () => {
+    setIntValue(42)
+  }
+
   return (
     <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
       <Header title="Example counter!"/>
-      <Counter intValue={42} />
-      <Counter intValue={1000}/>
-      <Counter/>
-      <Header title="Example Text input"/>
-      <TextInput/>
-      <Footer>
-        Footer content
-        <a href="https://google.com">Link</a>
-      </Footer>
+      <div style={{marginBottom: '40px'}}>
+      <button onClick={handleClick}>
+        {show ? 'Hide' : 'Show'}
+      </button>
+      <button onClick={handleClickChangeValue}>
+        set new initial value
+      </button>
+      </div>
+      {show && <Counter intValue={intValue} />}
+      {/*<Counter intValue={1000}/>*/}
+      {/*<Counter/>*/}
+      {/*<Header title="Example Text input"/>*/}
+      {/*<TextInput/>*/}
+      {/*<Footer>*/}
+      {/*  Footer content*/}
+      {/*  <a href="https://google.com">Link</a>*/}
+      {/*</Footer>*/}
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,94 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {CounterControls} from "./CounterControls";
 import {CounterValue} from "./CounterValue";
 
+
+const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, []);
+}
+
+const useUnMount = (callback) => {
+  useEffect(() => {
+    return () => {
+      callback()
+    }
+  }, []);
+}
+
 export const Counter = ({intValue = 0}) => {
   const [count, setCount] = useState(intValue);
+  const [value, setValue] = useState(0);
+
+  useMount(() => {
+    console.log('Mount custom')
+    // fetch('https://jsonplaceholder.typicode.com/todos/')
+    //   .then(response => response.json())
+    //   .then(json => console.log(json))
+    // setValue(100)
+  })
+
+  useUnMount(() => {
+    console.log('UnMount custom')
+  })
+
+  useEffect(function () {
+    console.log('Mount')
+
+    return () => {
+      console.log('Unmount')
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   const mouseMove = (e) => {
+  //     console.log(count, e.clientX, e.clientY)
+  //   }
+  //   document.addEventListener('mousemove', mouseMove);
+  //   return () => {
+  //     document.removeEventListener('mousemove', mouseMove)
+  //   }
+  // }, [count]);
+
+
+  // useEffect(() => {
+  //   console.log('intValue', intValue)
+  //   setCount(intValue)
+  //   return () => {
+  //     console.log('intValue return', intValue)
+  //   }
+  // }, [intValue]);
+  //
+  // useEffect(() => {
+  //   console.log('useEffect count: ', count)
+  //   if(count % 2 === 0) {
+  //     console.log('Кратно двум!')
+  //   }
+  //   return () => {
+  //     console.log('useEffect count return', count)
+  //   }
+  // }, [count]);
+
+  // useEffect(() => {
+  //   console.log({count, value})
+  // }, [count, value])
+
+
+  // if(count > 10) {
+  //   console.log('Счетчик больше 10', {count, value})
+  // }
+  console.log('Rerender')
 
   return (
     <>
       <CounterControls setCount={setCount} intValue={intValue}/>
       <CounterValue count={count}/>
+      <div>
+        Второй счетчик
+      </div>
+      <CounterControls setCount={setValue} intValue={0}/>
+      <CounterValue count={value}/>
     </>
   )
 }
